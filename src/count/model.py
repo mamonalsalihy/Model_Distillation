4c896e163191531771e3fae9963824fa72bea77c
mamon <mamonalsalihy@gmail.com>
2021-05-15 16:21:35 -0700

    updating model

--- src/count/model.py
+++ src/count/model.py
@@ -1,20 +1,22 @@
 # STL
 from typing import Dict
 
+import numpy
+
 # Utilities
 import torch
-import numpy
 from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
 
 # AllenNLP
 from allennlp.data import Instance, Token, Vocabulary
 from allennlp.data.data_loaders import SimpleDataLoader
-from allennlp.data.fields import TextField, LabelField
+from allennlp.data.fields import LabelField, TextField
 from allennlp.data.fields.text_field import TextFieldTensors
-from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
 
 # Models
 from allennlp.models import Model
+from allennlp.modules import Embedding, TextFieldEmbedder
 
 # Inference
 from allennlp.predictors.predictor import Predictor
@@ -25,7 +27,7 @@ from allennlp.training.trainer import GradientDescentTrainer, Trainer
 
 # Layers
 from allennlp.modules.attention import Attention
-from allennlp.modules.transformer import TransformerLayer
+from allennlp.modules.transformer import TransformerLayer, TransformerStack
 from allennlp.modules import Embedding, TextFieldEmbedder
 from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.nn.activations import Activation
@@ -38,27 +40,27 @@ import config
 @Model.register("language-model")
 class LanguageModel(Model):
     def __init__(
-            self,
-            vocab: Vocabulary,
-            embedder: TextFieldEmbedder,
-            hidden_size: int,
-            intermediate_size: int,
-            num_attention_heads: int,
-            att_dropout: float = 0.2,
-            hidden_dropout: float = 0.2,
-            activation: str = "relu",
-            cross_attention: bool = False,
+        self,
+        vocab: Vocabulary,
+        embedder: TextFieldEmbedder,
+        hidden_size: int,
+        intermediate_size: int,
+        num_attention_heads: int,
+        att_dropout: float = 0.2,
+        hidden_dropout: float = 0.2,
+        activation: str = "relu",
+        cross_attention: bool = False,
     ) -> None:
         super().__init__(vocab)
 
         self.embedder = embedder
         self.activation = Activation.by_name(activation)()
         # question: what is intermediate size?
-        self.transformer = TransformerLayer(
+        self.transformer = TransformerStack(
+            num_hidden_layers=4,
             hidden_size=hidden_size,
             intermediate_size=intermediate_size,
             num_attention_heads=num_attention_heads,
-            attention_dropout=att_dropout,
             hidden_dropout=hidden_dropout,
             activation=self.activation,
             add_cross_attention=cross_attention,
@@ -81,7 +83,7 @@ class LanguageModel(Model):
         token_ids = tokens["tokens"]["tokens"]
 
         # get source and targets from tokens
-        source = token_ids[:, 0:-1]
+        source = token_ids[:, :-1]
         target = token_ids[:, 1:]
 
         # do embedding stuff here
@@ -106,9 +108,11 @@ class LanguageModel(Model):
         preds = logits.reshape(-1, self.vocab_size)
         target = target.reshape(-1)
 
-        # need to pass pad idx so we can ignore this, unsure how to achieve this in AllenNLP
-        temp = torch.nn.functional.cross_entropy(preds, target, reduction='sum')
+        PAD_IDX = self.vocab.get_token_index(config.PAD)
+
+        temp = torch.nn.functional.cross_entropy(preds, target, ignore_index=PAD_IDX, reduction='sum')
         loss = temp / self.normalizer
+
 
         new_normalized = temp / (self.normalizer * self.dif_tokenizers_ratio)
 
@@ -118,7 +122,7 @@ class LanguageModel(Model):
         return {"logits": logits, "loss": loss, "probs": probs}
 
     def make_output_human_readable(
-            self, output_dict: Dict[str, torch.Tensor]
+        self, output_dict: Dict[str, torch.Tensor]
     ) -> Dict[str, torch.Tensor]:
         """Takes logits from `forward` and computes the corresponding label
 
@@ -131,10 +135,10 @@ class LanguageModel(Model):
         Dict[str, torch.Tensor]:
             Same as input dictionary, but with another key `label` indicating the predicted label
         """
-        # Take the logits from the forward pass, and compute the label IDs for maximum values
+        # Take the logits from the forward pass, and compute the label
+        # IDs for maximum values
         logits = output_dict["logits"].cpu().data.numpy()
         predicted_id: numpy.ndarray = numpy.argmax(logits, axis=-1)
-
         # Convert these IDs back to label strings using vocab
         output_dict["label"] = [
             self.vocab.get_token_from_index(x, namespace="tokens") for x in predicted_id
@@ -144,7 +148,5 @@ class LanguageModel(Model):
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         return {"perplexity": self.metric.get_metric(reset)}
 
-
-if __name__ == "__main__":
-    # all of these experiments are in main now
-    print("Run main.py for model training")
+    def count_parameters(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)

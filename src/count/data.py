9c97590bd84d0478e1e2abf258eb2621c327957f
offendo <nilaypatel2@gmail.com>
2021-05-20 14:36:31 -0400

    Make data loader load in partial sequences as well.
    Before, the first sequence it loads was
    `[<bos>, 1, 2, ..., context_size]`
    
    Now it loads
    `[<bos>, 2]`
    `[<bos>, 2, 3]`
    `[<bos>, 2, 3, 4]`
    etc.

--- src/count/data.py
+++ src/count/data.py
@@ -9,6 +9,8 @@ from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 # Data types
 from allennlp.data.fields import Field, TextField
 from allennlp.data.instance import Instance
+from allennlp.data import Vocabulary
+from allennlp.data.data_loaders import SimpleDataLoader
 
 # Indexers
 from allennlp.data.token_indexers import SingleIdTokenIndexer
@@ -47,19 +49,17 @@ class WikiTextReader(DatasetReader):
     """
 
     def __init__(
-            self,
-            context: int,
-            tokenizer: Tokenizer = None,
-            token_indexers: Dict[str, TokenIndexer] = None,
-            **kwargs,
+        self,
+        context: int,
+        tokenizer: Tokenizer = None,
+        token_indexers: Dict[str, TokenIndexer] = None,
+        **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         self._sentence_splitter = SpacySentenceSplitter(rule_based=True)
         self._tokenizer = tokenizer or WhitespaceTokenizer()
         self._targets_tokenizer = self._tokenizer
-        self._token_indexers = token_indexers or {
-            "tokens": SingleIdTokenIndexer(namespace="tokens")
-        }
+        self._token_indexers = token_indexers or {"token": SingleIdTokenIndexer(namespace="tokens")}
         self._context = context
 
     def _single_process_read(self, file_path: str) -> Iterable[Instance]:
@@ -91,9 +91,14 @@ class WikiTextReader(DatasetReader):
         sentences = self._sentence_splitter.split_sentences(text)
         for sent in sentences:
             tokens = self._tokenizer.tokenize(sent)
-            for start in range(len(tokens) - self._context):
-                width = start + self._context
-                yield self.text_to_instance(tokens[start: width + 1])
+            # tokens = bos, 1, 2, ..., n, eos
+            for end in range(2, len(tokens) + 1):
+                # the last token should range from token 1 --> eos
+                # the first token should range from
+                # bos --> `context` tokens before eos (i.e., n - `context` + 1)
+                start = max(end - self._context, 0)
+                context = tokens[start:end]
+                yield self.text_to_instance(context)
 
     def text_to_instance(
         self,
@@ -112,8 +117,8 @@ class WikiTextReader(DatasetReader):
             Instance containing a `tokens` field and a `target` field.
         """
 
-        input_field = TextField(tokens)
-        fields: Dict[str, Field] = {"tokens": input_field}
+        tokens = TextField(tokens)
+        fields: Dict[str, Field] = {"tokens": tokens}
         return Instance(fields)
 
     def apply_token_indexers(self, instance):
@@ -129,8 +134,19 @@ if __name__ == "__main__":
     reader = WikiTextReader(
         context=10,
         tokenizer=wiki_tokenizer,
-        token_indexers={"tokens": SingleIdTokenIndexer(namespace="tokens")},
+        token_indexers={
+            "tokens": SingleIdTokenIndexer(namespace="tokens"),
+        },
+        max_instances=20,
+        manual_multiprocess_sharding=True,
+        manual_distributed_sharding=True,
     )
-    dataset = reader.read(os.path.join(config.WIKI_RAW_DIR, "wiki.train.raw"))
-    for i in islice(dataset, 4):
+    train_data_loader = SimpleDataLoader(
+        instances=list(reader.read(os.path.join(config.WIKI_RAW_DIR, "wiki.train.raw"))),
+        batch_size=1,
+        shuffle=False,
+    )
+    vocab = Vocabulary.from_files(config.VOCAB_DIR, padding_token=config.PAD, oov_token=config.UNK)
+    train_data_loader.index_with(vocab)
+    for i in train_data_loader:
         print(i)

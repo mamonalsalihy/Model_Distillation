7408e14ebe07c7270f20d00fa11dc9b3a5e5d121
offendo <nilaypatel2@gmail.com>
2021-07-10 14:55:40 -0400

    Add word-level perplexity calculation

--- src/count/data.py
+++ src/count/data.py
@@ -17,7 +17,7 @@ from allennlp.data.data_loaders import MultiProcessDataLoader
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 
 # Data types
-from allennlp.data.fields import Field, TextField, MetadataField, TensorField
+from allennlp.data.fields import Field, TextField, FlagField, TensorField
 from allennlp.data.instance import Instance
 
 # Indexers
@@ -92,9 +92,11 @@ class WikiTextReader(DatasetReader):
             with open(cache, "wb") as f:
                 torch.save(dataset, f)
 
-        yield from self.tensor_to_instances(dataset["subwords"])
+        # ratio to use when calculating word level perplexity
+        ratio = len(dataset["subwords"]) / sum(dataset["num_words"])
+        yield from self.tensor_to_instances(dataset["subwords"], ratio.item())
 
-    def tensor_to_instances(self, subwords: torch.Tensor):
+    def tensor_to_instances(self, subwords: torch.Tensor, ratio: float):
         logger.info("Building instances...")
         if self.lstm:
             eos_idx = self.tokenizer.token_to_id("[SEP]")
@@ -102,18 +104,18 @@ class WikiTextReader(DatasetReader):
             start = 0
             for i, end in enumerate(sequence_indices):
                 seq = subwords[start : end + 1]
-                yield Instance({"tokens": TensorField(seq)})
+                yield Instance({"tokens": TensorField(seq), "ratio": FlagField(ratio)})
                 start = end + 1
         elif self.exclusive:
             num_sequences = (subwords.size(0) // self.sequence_length) * self.sequence_length
             sequences = subwords.narrow(0, 0, num_sequences).view(-1, self.sequence_length)
             for inst in sequences:
-                yield Instance({"tokens": TensorField(inst)})
+                yield Instance({"tokens": TensorField(inst), "ratio": FlagField(ratio)})
         else:
             for end_idx in range(1, len(subwords)):
                 start_idx = max(0, end_idx - self.sequence_length)
                 inst = subwords[start_idx : end_idx + 1]
-                yield Instance({"tokens": TensorField(inst)})
+                yield Instance({"tokens": TensorField(inst), "ratio": FlagField(ratio)})
 
     def text_to_instance(
         self,
@@ -121,7 +123,8 @@ class WikiTextReader(DatasetReader):
     ) -> Instance:
         tokens = torch.tensor(self.tokenizer(text).ids, dtype=torch.long)
         num_words = len(text.split())
-        return Instance({"tokens": TensorField(tokens)})
+        ratio = len(tokens) // num_words
+        return Instance({"tokens": TensorField(tokens), "ratio": FlagField(ratio)})
 
     def apply_token_indexers(self, instance) -> None:
         """Adds a token indexer to the instance. Automatically called by AllenNLP."""
@@ -135,7 +138,7 @@ if __name__ == "__main__":
         tokenizer_path=config.TOKENIZER,
         max_instances=None,
         lstm=False,
-        exclusive=False,
+        exclusive=True,
     )
 
     loader = MultiProcessDataLoader(
@@ -151,5 +154,5 @@ if __name__ == "__main__":
         input()
 
     # Valid ratio: 1.1494
-    # Test ratio: 1.1537
+    # Test ratio:  1.1537
     # Train ratio: 1.1633
--- src/count/models/base_transformer.py
+++ src/count/models/base_transformer.py
@@ -12,7 +12,6 @@ import torch.nn as nn
 
 # AllenNLP
 from allennlp.data import Vocabulary
-from allennlp.data import TensorDict
 
 # Models
 from allennlp.models import Model
@@ -65,7 +64,8 @@ class Transformer(Model):
 
         # Evaluation
         # ==========
-        self.metric = Perplexity()
+        self.perplexity = Perplexity()
+        self.word_perplexity = Perplexity()
         self.loss = nn.CrossEntropyLoss(ignore_index=self.PAD_IDX, reduction="mean")
 
         # Initialize weights
@@ -104,7 +104,8 @@ class Transformer(Model):
 
     def forward(
         self,
-        tokens: TensorDict,
+        tokens: torch.Tensor,
+        ratio: float,
         only_predict_next: bool = False,
     ) -> Dict[str, torch.Tensor]:
         tokens = tokens.transpose(0, 1)  # new shape [S+1, B]
@@ -130,7 +131,8 @@ class Transformer(Model):
         reals = labels.reshape(-1)
         loss = self.loss(preds, reals)
 
-        self.metric(loss)
+        self.perplexity(loss)
+        self.word_perplexity(loss * ratio)
         return {"logits": logits, "loss": loss}
 
     def make_output_human_readable(
@@ -160,7 +162,8 @@ class Transformer(Model):
 
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         return {
-            "perplexity": self.metric.get_metric(reset),
+            "perplexity": self.perplexity.get_metric(reset),
+            "word_perplexity": self.word_perplexity.get_metric(reset),
         }
 
     def count_parameters(self):

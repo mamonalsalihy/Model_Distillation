24b56abaf15c1768f0da6a2b60f985b93ed61888
Austin King <aukking@ucsc.edu>
2021-07-23 09:40:45 -0700

    debugging bidirectional model

--- src/count/models/dual_directional.py
+++ src/count/models/dual_directional.py
@@ -77,14 +77,14 @@ class DualDirectionalModel(Model):
         backward = self.backward_model.forward(tokens, ratio)
 
         forward_logits = forward["logits"]  # Logits for tokens 2 -> N
-        backward_logits = torch.flip(backward["logits"], dims=[0])  # Logits for tokens 1 -> N-1
+        backward_logits = torch.flip(backward["logits"], dims=[1])  # Logits for tokens 1 -> N-1
 
         print(forward_logits[:-1, :, :].size())
         print(backward_logits[1:, :, :].size())
         print('=========================')
-        for x, y in zip(torch.argmax(forward_logits[:-1, :, :], dim=0).view(-1),
-                        torch.argmax(backward_logits[1:, :, :], dim=0).view(-1)):
-            print("%d \t %d" % (x.item(), y.item()))
+        # for x, y in zip(torch.argmax(forward_logits[:-1, :, :], dim=0).view(-1),
+        #                 torch.argmax(backward_logits[1:, :, :], dim=0).view(-1)):
+        #     print("%d \t %d" % (x.item(), y.item()))
 
         # we don't need to consider the logits for the first token
         # we need to weight logits 2 -> N-1

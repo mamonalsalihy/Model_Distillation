ea51344a2ebf602f40d2e5902f6334dc17dfd23a
mamon <mamonalsalihy@gmail.com>
2021-08-04 19:55:43 -0700

    Revert "temperature setting as hyperparam, dropdown-functionality, MKD config files"
    This reverts commit 42d0cbde

--- flask_demo/contents/create_app.py
+++ flask_demo/contents/create_app.py
@@ -1,12 +1,10 @@
 import argparse
-import json
 import os
-from flask import Flask, render_template, request, Response, redirect, url_for, jsonify
+from flask import Flask, render_template, request
 # STL
 import sys
 from pathlib import Path
 import argparse
-import pickle
 
 # Torch
 import torch
@@ -57,23 +55,16 @@ def create_app(args, test_config=None):
     except OSError:
         pass
 
+    inf = init_model(args)
+
     # Accesses the root URL
     @app.route('/', methods=["POST", "GET"])
     def root():
         if request.method == "POST":
-            if request.form.get('submit_button') == "baseline":
-                args.archive_dir = '../experiments/16-layer'
-            if request.form.get('submit_button') == 'best_kd':
-                args.archive_dir = '../experiments/138M-model'
-            if request.form.get('submit_button') == 'the_count':
-                args.archive_dir = '../experiments/count'
-            inf = init_model(args)
             input_query = request.form.get("query")
             if input_query == '':
                 return render_template('index.html', query="Please input a query")
-            if input_query is None:
-                return render_template('index.html', query='Model was switched!')
-            count_response = inf.speak(input_query, int(args.max), float(args.temperature))
+            count_response = inf.speak(input_query, int(args.max))
             return render_template('index.html', query=count_response)
         else:
             return render_template("index.html")
